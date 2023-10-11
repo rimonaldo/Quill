27a59c5687f473bc897a4dba522f2f830db0a574
rimonaldo <rimons2804@gmail.com>
2023-10-11 18:55:13 -0400

    added landing page blury gradient element

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,6 @@
-import clsx, { ClassValue } from 'clsx'
-import { twMerge } from 'tailwind-merge'
-
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+ 
 export function cn(...inputs: ClassValue[]) {
-    /* twMerge is a function that merges tailwind classes
-        for example: 
-        twMerge('text-red-500', 'bg-blue-500') => 'text-red-500 bg-blue-500'*/
-    return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs))
 }
